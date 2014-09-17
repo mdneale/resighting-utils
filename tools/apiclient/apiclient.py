1c7437667fd468ec663e6dab1fc15ddbb81d522a
Matthew Neale <matt@matthewneale.net>
2014-09-17 20:04:27 +0200

    Adding support for ListUserLocalityStatistics method

--- tools/apiclient/apiclient.py
+++ tools/apiclient/apiclient.py
@@ -54,6 +54,7 @@ Methods:
   ListSightingLocators
   ListSightings
   ListUserCountryStatistics
+  ListUserLocalityStatistics
   ListUserLocators
   ListUserSightings
   Meta
@@ -716,6 +717,39 @@ def api_listusercountrystatistics(server_url, opts):
 
     return method_url, None, None
 
+def api_listuserlocalitystatistics(server_url, opts):
+    """Construct the url for a call to the ListUserLocalityStatistics API method.
+    
+    Arguments:
+    server_url - The url of the server where the API is running.
+    opts - The command-line options.
+    
+    Returns:
+    A tuple containing the full url for invoking the API method and None for
+    the POST data and content type as this is a GET request.
+    
+    Raises:
+    Error if no user_id was specified on the command-line.
+    """
+    # The url requires a user_id so this is mandatory
+    if opts.user_id is None:
+        raise Error('A user-id is required for this API method')
+    
+    params = {}
+    
+    if opts.access_token is not None:
+        params['access_token'] = opts.access_token
+
+    if opts.cursor is not None:
+        params['cursor'] = opts.cursor
+
+    if opts.fetch_size is not None:
+        params['fetch_size'] = opts.fetch_size
+
+    method_url = '%s/%s/users/%s/statistics/localities?%s' % (server_url, _API_ROOT_PATH, opts.user_id, urllib.urlencode(params))
+
+    return method_url, None, None
+
 def api_listuserlocators(server_url, opts):
     """Construct the url for a call to the ListUserLocators API method.
     
@@ -1089,6 +1123,7 @@ methods = {
     'listsightinglocators': api_listsightinglocators,
     'listsightings': api_listsightings,
     'listusercountrystatistics': api_listusercountrystatistics,
+    'listuserlocalitystatistics': api_listuserlocalitystatistics,
     'listuserlocators': api_listuserlocators,
     'listusersightings': api_listusersightings,
     'meta': api_meta,
@@ -1128,6 +1163,7 @@ Methods:
   ListSightingLocators
   ListSightings
   ListUserCountryStatistics
+  ListUserLocalityStatistics
   ListUserLocators
   ListUserSightings
   Meta
